aefe2b9cfa5acd833ca31a0d65367decf42ea35f
kozzzz-07 <kozzzz-07@noreply>
2020-10-11 22:58:57 +0900

    main-nav 見た目

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainNavComponent } from './main-nav/main-nav.component';
-
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [MainNavComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule],
 })
-export class SharedModule { }
+export class SharedModule {}
