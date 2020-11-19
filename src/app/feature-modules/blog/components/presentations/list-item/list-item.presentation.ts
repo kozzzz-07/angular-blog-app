ed844daf652b181cd91e9e07e88414dc145f2df8
kozzzz-07 <kozzzz-07@noreply>
2020-11-20 01:45:06 +0900

    list-item.presentation input

--- src/app/feature-modules/blog/components/presentations/list-item/list-item.presentation.ts
+++ src/app/feature-modules/blog/components/presentations/list-item/list-item.presentation.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-presentation-list-item',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-item.presentation.scss'],
 })
 export class ListItemPresentationComponent implements OnInit {
+  @Input() title!: string;
+  @Input() date!: Date;
+  @Input() excerpt!: string;
+
   constructor() {}
 
   ngOnInit(): void {}
