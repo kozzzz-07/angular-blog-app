0eb1f9e7462c97affafa56ad1e982e1b14b5c3de
kozzzz-07 <kozzzz-07@noreply>
2020-11-20 03:24:42 +0900

    card にViewEncapsulation.ShadowDom

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class CardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
