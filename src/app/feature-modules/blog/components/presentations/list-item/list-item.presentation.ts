257b251064915772db381f09be6fe5c73980be2b
kozzzz-07 <kozzzz-07@noreply>
2020-11-20 18:33:54 +0900

    ボタン装飾

--- src/app/feature-modules/blog/components/presentations/list-item/list-item.presentation.ts
+++ src/app/feature-modules/blog/components/presentations/list-item/list-item.presentation.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-presentation-list-item',
   templateUrl: './list-item.presentation.html',
   styleUrls: ['./list-item.presentation.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ListItemPresentationComponent implements OnInit {
   @Input() title!: string;
