85e3023cea38f5fa44782878c08ef5f429ca6946
kozzzz-07 <kozzzz-07@noreply>
2020-11-29 22:50:39 +0900

    add ボタンコンポーネント

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent implements OnInit {
+  @Input() type = 'button';
+  @Input() disabled = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { RouterModule } from '@angular/router';
 import { CardComponent } from './card/card.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  declarations: [MainNavComponent, CardComponent],
+  declarations: [MainNavComponent, CardComponent, ButtonComponent],
   imports: [CommonModule, RouterModule],
-  exports: [CardComponent],
+  exports: [CardComponent, ButtonComponent],
 })
 export class SharedModule {}
